26f8a9d87d18e085da5e184fdb69783690c9d82e
sasha <suslovru85@gmail.com>
2026-02-07 21:00:49 +0300

    my second commit

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ class User:
             return 'Сначала получи паспорт, а потом уже баб ***'
         else:
             return 'Красавчик, можешь проходить на тусу'
+
+print('Added something new, just check the commit')
