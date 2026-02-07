2e6f9db9cd6af89f800d73404a574db6323a07fc
sasha <suslovru85@gmail.com>
2026-02-07 22:14:18 +0300

    version 2.0.0.

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-class User:
-    def __init__(self, name, age):
-        self.name = name 
-        self.age = age 
+class Vehicle:
+    def __init__(self, color, consumption, tank_volume, mileage=0):
+        self.color = color 
+        self.consumption = consumption
+        self.tank_volume = tank_volume
+        self.reserve = tank_volume
+        self.mileage = mileage
+        self.engine_on = True 
 
-    def age_control(self):
-        if self.age < 18:
-            return 'Сначала получи паспорт, а потом уже баб ***'
-        else:
-            return 'Красавчик, можешь проходить на тусу'
+    def start_engine(self):
+        if self.engine_on == False and self.reserve > 0:
+            self.engine_on = True 
+            return 'Engine on'
+        return 'Error, engine cant be launched because engine is already started or fuel supply is empty'
+    
+    def stop_engine(self):
+        if self.engine_on == True:
+            self.engine_on = False 
+            return 'Engine off'
+        return 'Error, engine has already turned off'
+    
+    def drive(self, distance):
+        if self.engine_on == False:
+            return 'Car cant drive because engine off'
+        if self.reserve / self.consumption * 100 < distance:
+            return 'Low fuel level'
+        self.mileage -= distance
+        self.reserve -= distance / self.consumption * 100 
+        return f'The car passed {distance} km. Fuel reserve {self.reserve} l.'
+    
+    def refuel(self):
+        self.reserve = self.tank_volume
 
-print('Added something new, just check the commit')
+    def get_mileage(self):
+        return f'Cars mileage {self.mileage}km.'
+    
+    def get_reserve(self):
+        return f'Fuel reserve: {self.reserve}l.'
+    
